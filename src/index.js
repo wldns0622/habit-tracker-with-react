0db79bf64231419f1f3a9f7b421c4a54c2b8c31f
wldns0622 <wldns12378@gmail.com>
2021-05-23 02:27:08 +0900

    test react hook(file: component/simpleHabit)

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import '@fortawesome/fontawesome-free/js/all';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import SimpleHabit from './component/simpleHabit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SimpleHabit />
   </React.StrictMode>,
   document.getElementById('root')
 );
